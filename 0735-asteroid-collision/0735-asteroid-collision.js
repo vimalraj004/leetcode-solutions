b2c20a93ed8c7329df8142f736a1426ffe60631e
Vimalraj J <vimalrajvj1048@gmail.com>
2026-08-01 18:54:24 +0530

    Time: 7 ms (23.25%), Space: 56 MB (86.59%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(asteroids) {
+    let stack = [];
+
+    for (let asteroid of asteroids) {
+        let alive = true;
+        while (
+            alive &&
+            stack.length &&
+            stack[stack.length - 1] > 0 &&
+            asteroid < 0
+        ) {
+
+            let top = stack[stack.length - 1];
+
+            if (Math.abs(top) < Math.abs(asteroid)) {
+                stack.pop();
+            }
+            else if (Math.abs(top) === Math.abs(asteroid)) {
+                stack.pop();
+                alive = false;
+            }
+            else {
+                alive = false;
+            }
+        }
+        if (alive) {
+            stack.push(asteroid);
+        }
+    }
+
+    return stack;
+};
