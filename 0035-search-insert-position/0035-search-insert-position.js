be2a06b5cf56e6c8a48541e9cc969f3b2e195330
Vimalraj J <vimalrajvj1048@gmail.com>
2026-08-29 17:54:52 +0530

    Time: 4 ms (1.78%), Space: 54.9 MB (9.34%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+let searchInsert =  (nums, target)=> {
+ 
+ let left = 0 , right = nums.length -1;
+let mid; 
+if(target < nums[left]){
+        return left; 
+    } 
+while (right>=left) {
+    console.log(left,"left",right,"right")
+     mid = Math.floor( (left + right) /2);
+    console.log(mid,"mid")
+    if(target > nums[nums.length-1]){
+        return nums.length-1 + 1;
+    }
+   else if(nums[mid] > target){
+        right = mid-1;
+    }else if(nums[mid] < target){
+        left = mid +1;
+    }
+    else{
+        return  mid ;
+    }
+}
+    console.log(mid,"outside mid")
+    return left;
+
+};
