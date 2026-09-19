0b48d7912d58747eadb422095444310c7d4832ab
Vimalraj J <vimalrajvj1048@gmail.com>
2026-09-19 21:56:32 +0530

    Time: 40 ms (69.43%), Space: 53.3 MB (67.77%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.js
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -18,17 +18,16 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        let left = 0, right = n , firstbadversion = 0;
+        let left = 1, right = n;
         while(left <=  right){
             let mid = Math.floor((left+right)/2);
         if(isBadVersion(mid) === false){
                 left = mid +1
         }else{
-            firstbadversion = mid;
             right = mid-1;
         }
 
         }
-        return firstbadversion
+        return left
     };
 };
